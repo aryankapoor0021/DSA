c42857c3fc659d3b97bff1aba1ec369da131f816
Aryan Kapoor <aryankapoorjagadhri@gmail.com>
2021-01-10 20:10:49 +0530

    Create 74. Search a 2D Matrix.java

---
+++ Matrix/74. Search a 2D Matrix.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i<matrix.length;i++)
+        {
+           int x=matrix[i]
+        }
+        return false;
+    }
+}
